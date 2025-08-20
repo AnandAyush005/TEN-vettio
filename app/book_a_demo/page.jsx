5f705febe5b5c4702c03657711f25281284d1f82
AnandAyush005 <anandayush233@gmail.com>
2025-08-20 10:46:55 +0530

    ..

---
+++ app/book_a_demo/page.jsx
@@ -0,0 +1,5 @@
+export default function Home(){
+    return <div>
+        hi there from book_a_demo
+    </div>
+}
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+  <div>
+    Hi there from home page
+  </div>
+  );
+}
